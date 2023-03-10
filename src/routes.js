a03a2ec77c294fc3eea0cf85dc769ef51529a0c9
MANSOURIFIRAS <mansouri.firas@esprit.tn>
2023-03-10 23:26:53 +0100

    architecture front office

---
+++ src/routes.js
@@ -0,0 +1,20 @@
+// import React, { lazy } from 'react';
+// import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import {Home}  from './pages/home';
+
+// const Products = lazy(() => import('./components/Products'));
+// const Composts = lazy(() => import('./components/Composts'));
+
+// const Routes = () => {
+//   return (
+//     <Router>
+//         <Switch>
+//           <Route exact path="/" component={Home} />
+//           <Route path="/Products" component={Products} />
+//           <Route path="/Products" component={Composts} />
+//         </Switch>
+//     </Router>
+//   );
+// };
+
+// export default Routes;
