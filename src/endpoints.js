19cba187ee73d835d07f62f090af9366db4eeab2
MANSOURIFIRAS <mansouri.firas@esprit.tn>
2023-05-01 17:05:31 +0200

    BIOUP-137-as-a-user-i-want-to-rate-a-compost-front-office done

--- src/endpoints.js
+++ src/endpoints.js
@@ -4,3 +4,4 @@ export const COMPOSTS_ENDPOINT = BASE_URL+'composts';
 export const PRODUCTS_ENDPOINT = BASE_URL+'products';
 export const SERVICES_ENDPOINT = BASE_URL+'services';
 export const COMMANDS_ENDPOINT = BASE_URL+'commands';
+export const RATINGS_ENDPOINT = BASE_URL+'rating';
